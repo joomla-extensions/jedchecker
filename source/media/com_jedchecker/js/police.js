f4011c1675fce8f867687a89b0aa7dc9238a1b16
Daniel Dimitrov <daniel@compojoom.com>
2012-06-09 14:02:45 +0200

    fixing problems with installation

---
+++ source/media/com_jedchecker/js/police.js
@@ -0,0 +1,7 @@
+/**
+ * @author Daniel Dimitrov - compojoom.com
+ * @date: 09.06.12
+ *
+ * @copyright  Copyright (C) 2008 - 2012 compojoom.com . All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
