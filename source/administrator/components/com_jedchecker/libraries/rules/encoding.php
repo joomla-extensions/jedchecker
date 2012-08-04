58834503e6b675fe2aad8de76d8f251c694a134a
Daniel Dimitrov <daniel@compojoom.com>
2012-08-04 20:48:04 +0200

    adding encoding rule - checks base64 usage

---
+++ source/administrator/components/com_jedchecker/libraries/rules/encoding.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * @author     Daniel Dimitrov
+ * @date       04/08/2012
+ * @copyright  Copyright (C) 2008 - 2012 compojoom.com . All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('_JEXEC') or die('Restricted access');
+
+
+// Include the rule base class
+require_once(JPATH_COMPONENT_ADMINISTRATOR.'/models/rule.php');
+
+
+/**
+ * This class searches all files for the _JEXEC check
+ * which prevents direct file access.
+ *
+ */
+class jedcheckerRulesEncoding extends JEDcheckerRule
+{
+    /**
+     * The formal ID of this rule. For example: SE1.
+     *
+     * @var    string
+     */
+    protected $id = 'encoding';
+
+    /**
+     * The title or caption of this rule.
+     *
+     * @var    string
+     */
+    protected $title = 'COM_JEDCHECKER_RULE_ENCODING';
+
+    /**
+     * The description of this rule.
+     *
+     * @var    string
+     */
+    protected $description = 'COM_JEDCHECKER_RULE_ENCODING_DESC';
+
+
+    /**
+     * Initiates the file search and check
+     *
+     * @return    void
+     */
+    public function check()
+    {
+        // Find all php files of the extension
+        $files = JFolder::files($this->basedir, '.php$', true, true);
+
+        // Iterate through all files
+        foreach($files as $file)
+        {
+            // Try to find the base64 use in the file
+            if($this->find($file)) {
+                // Add as error to the report if it was not found
+                $this->report->addError($file, 'COM_JEDCHECKER_ERROR_ENCODING');
+            }
+        }
+    }
+
+
+    /**
+     * Reads a file and searches for any encoding function defined in the params
+     * Not a very clever way of doing this, but it should be fine for now
+     *
+     * @param     string    $file    The path to the file
+     * @return    boolean            True if the statement was found, otherwise False.
+     */
+    protected function find($file)
+    {
+        $content = (array) file($file);
+
+        // Get the functions to look for
+        $encodings = explode(',', $this->params->get('encodings'));
+
+        foreach($encodings as $encoding) {
+            $encoding = trim($encoding);
+            foreach ($content AS $line)
+            {
+                // Search for "base64"
+                $pos_1 = stripos($line, $encoding);
+
+                if ($pos_1 !== false) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
