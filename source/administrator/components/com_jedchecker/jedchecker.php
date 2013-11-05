6792e66b073d2cb9c25d50495604c4af773be57a
Daniel Dimitrov <daniel@compojoom.com>
2013-11-05 20:22:50 +0100

    # JFile not found

--- source/administrator/components/com_jedchecker/jedchecker.php
+++ source/administrator/components/com_jedchecker/jedchecker.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @author Daniel Dimitrov - compojoom.com
- * @date: 02.06.12
+ * @author     Daniel Dimitrov - compojoom.com
+ * @date       : 02.06.12
  *
  * @copyright  Copyright (C) 2008 - 2012 compojoom.com . All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE
@@ -10,11 +10,18 @@
 defined('_JEXEC') or die('Restricted access');
 jimport('joomla.application.component.controllerlegacy');
 
+// We'll need jfile and JFolder all through the compoenent so let us load them here
+jimport('joomla.filesystem.folder');
+jimport('joomla.filesystem.file');
+
 $input = JFactory::getApplication()->input;
-$view = $input->getCmd('view','');
-if($view == '' && $input->getCmd('task', '') == '') {
+$view = $input->getCmd('view', '');
+
+if ($view == '' && $input->getCmd('task', '') == '')
+{
 	$input->set('view', 'uploads');
 }
+
 $controller = JControllerLegacy::getInstance('jedchecker');
-$controller->execute($input->getCmd('task',''));
+$controller->execute($input->getCmd('task', ''));
 $controller->redirect();
