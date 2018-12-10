22df11914b812aa40cd95c9b79c827b7700bdc67
Texpaok <texpaok@hotmail.com>
2018-12-10 14:00:03 +0100

    Add files via upload

--- administrator/components/com_jedchecker/script.php
+++ administrator/components/com_jedchecker/script.php
@@ -32,7 +32,7 @@ class Com_JedcheckerInstallerScript
 		{
 			$this->loadLanguage();
 			
-			Jerror::raiseWarning(null, JText::sprintf('COM_JEDCHECKER_PHP_VERSION_INCOMPATIBLE', PHP_VERSION, '5.3.10'));
+			JFactory::getApplication()->enqueueMessage(JText::sprintf('COM_JEDCHECKER_PHP_VERSION_INCOMPATIBLE', PHP_VERSION, '5.3.10'),'error');
 
 			return false;
 		}
