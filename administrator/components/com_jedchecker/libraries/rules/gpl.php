02ccd6fa65ef5e61ad701d9da1a64fa338224640
Denis Ryabov <dryabov@yandex.ru>
2021-01-31 11:52:32 +0300

    move lists of GPL and compatible licenses to separate files

--- administrator/components/com_jedchecker/libraries/rules/gpl.php
+++ administrator/components/com_jedchecker/libraries/rules/gpl.php
@@ -48,12 +48,29 @@ class JedcheckerRulesGpl extends JEDcheckerRule
 	protected $description = 'COM_JEDCHECKER_RULE_PH1_DESC';
 
 	/**
+	 * Regular expression to match GPL licenses.
+	 *
+	 * @var    string
+	 */
+	protected $regex_gpl_licenses;
+
+	/**
+	 * Regular expression to match GPL-compatible licenses.
+	 *
+	 * @var    string
+	 */
+	protected $regex_compat_licenses;
+
+	/**
 	 * Initiates the file search and check
 	 *
 	 * @return    void
 	 */
 	public function check()
 	{
+		// Prepare regexp
+		$this->init();
+
 		// Find all php files of the extension
 		$files = JFolder::files($this->basedir, '.php$', true, true);
 
@@ -70,7 +87,94 @@ class JedcheckerRulesGpl extends JEDcheckerRule
 	}
 
 	/**
-	 * Reads a file and searches for the _JEXEC statement
+	 * Initialization (prepare regular expressions)
+	 */
+	protected function init()
+	{
+		$gpl_licenses = (array) file(__DIR__ . '/gpl_gnu.txt');
+		$this->regex_gpl_licenses = $this->generate_regexp($gpl_licenses);
+
+		$compat_licenses = (array) file(__DIR__ . '/gpl_compat.txt');
+
+		$extra_licenses = $this->params->get('constants');
+		$extra_licenses = explode(',', $extra_licenses);
+
+		$compat_licenses = array_merge($compat_licenses, $extra_licenses);
+
+		$this->regex_compat_licenses = $this->generate_regexp($compat_licenses);
+	}
+
+	/**
+	 * Generate regular expression to match the given list of license names
+	 * @param $lines
+	 * @return string
+	 */
+	protected function generate_regexp($lines)
+	{
+		$titles = array();
+		$ids = array();
+
+		foreach ($lines as $line)
+		{
+			$line = trim($line);
+			if ($line === '' || $line[0] === '#')
+			{
+				// skip empty and commented lines
+				continue;
+			}
+
+			$title = $line;
+			if (substr($line, -1, 1) === ')')
+			{
+				// extract identifier
+				$pos = strrpos($line, '(');
+				if ($pos !== false)
+				{
+					$title = trim(substr($line, 0, $pos));
+
+					$id = trim(substr($line, $pos + 1, -1));
+
+					if ($id !== '')
+					{
+						$id = preg_quote($id, '#');
+						$ids[$id] = 1;
+					}
+				}
+			}
+
+			if ($title !== '')
+			{
+				$title = preg_quote($title, '#');
+
+				// expand vN.N to different version formats
+				$title = preg_replace('/(?<=\S)\s+v(?=\d)/', ',?\s+(?:v\.?\s*|version\s+)?', $title);
+
+				$title = preg_replace('/\s+/', '\s+', $title);
+
+				$titles[$title] = 1;
+			}
+		}
+
+		if (count($titles) === 0)
+		{
+			return null;
+		}
+
+		$titles = implode('|', array_keys($titles));
+
+		if (count($ids))
+		{
+			$ids = implode('|', array_keys($ids));
+			$titles .=
+				'|\blicense\b.+?(?:' . $ids . ')' .
+				'|\b(?:' . $ids . ')\s+license\b';
+		}
+
+		return '#^.*?(?:' . $titles . ').*?$#im';
+	}
+
+	/**
+	 * Reads a file and searches for its license
 	 *
 	 * @param   string  $file  - The path to the file
 	 *
@@ -78,72 +182,37 @@ class JedcheckerRulesGpl extends JEDcheckerRule
 	 */
 	protected function find($file)
 	{
-		$content = (array) file($file);
-
-		// Get the constants to look for
-		$licenses = $this->params->get('constants');
-		$licenses = explode(',', $licenses);
-
-		$hascode = 0;
-
-		foreach ($content AS $key => $line)
+		// check the file is empty (i.e. comments-only)
+		$content = php_strip_whitespace($file);
+		if (preg_match('#^<\?php\s+$#', $content))
 		{
-			$tline = trim($line);
-
-			if ($tline == '' || $tline == '<?php' || $tline == '?>')
-			{
-				continue;
-			}
-
-			if ($tline['0'] != '/' && $tline['0'] != '*')
-			{
-				$hascode = 1;
-			}
-
-			// Search for GPL license
-			$gpl = stripos($line, 'GPL');
-			$gnu = stripos($line, 'GNU');
-			$gpl_long = stripos($line, 'general public license');
-
-			if ($gpl || $gnu || $gpl_long)
-			{
-				$this->report->addInfo(
-					$file,
-					JText::_('COM_JEDCHECKER_PH1_LICENSE_FOUND') . ':' . '<strong>' . $line . '</strong>',
-					$key
-				);
-
-				return true;
-			}
-
-			// Search for the constant name
-			foreach ($licenses AS $license)
-			{
-				$license = trim($license);
-
-				// Search for the license
-				$found = strpos($line, $license);
-
-				// Skip the line if the license is not found
-				if ($found === false)
-				{
-					continue;
-				}
-				else
-				{
-					$this->report->addInfo(
-						$file,
-						JText::_('COM_JEDCHECKER_GPL_COMPATIBLE_LICENSE_WAS_FOUND') . ':' . '<strong>' . $line . '</strong>',
-						$key
-					);
-
-					return true;
-				}
-			}
+			return true;
 		}
 
-		unset($content);
+		$content = file_get_contents($file);
 
-		return $hascode ? false : true;
+		if (preg_match($this->regex_gpl_licenses, $content, $match, PREG_OFFSET_CAPTURE))
+		{
+			$line_no = substr_count($content, "\n", 0, $match[0][1]) + 1;
+			$this->report->addInfo(
+				$file,
+				JText::_('COM_JEDCHECKER_PH1_LICENSE_FOUND') . ':' . '<strong>' . $match[0][0] . '</strong>',
+				$line_no
+			);
+			return true;
+		}
+
+		if (preg_match($this->regex_compat_licenses, $content, $match, PREG_OFFSET_CAPTURE))
+		{
+			$line_no = substr_count($content, "\n", 0, $match[0][1]) + 1;
+			$this->report->addInfo(
+				$file,
+				JText::_('COM_JEDCHECKER_GPL_COMPATIBLE_LICENSE_WAS_FOUND') . ':' . '<strong>' . $match[0][0] . '</strong>',
+				$line_no
+			);
+			return true;
+		}
+
+		return false;
 	}
 }
