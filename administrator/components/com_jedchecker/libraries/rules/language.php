6e6b1224a00fa7f64f08bfa105fe03645f268c0f
Denis Ryabov <dryabov@yandex.ru>
2021-03-09 23:50:26 +0300

    Check file is correctly read

--- administrator/components/com_jedchecker/libraries/rules/language.php
+++ administrator/components/com_jedchecker/libraries/rules/language.php
@@ -93,6 +93,12 @@ class JedcheckerRulesLanguage extends JEDcheckerRule
 		}
 
 		$lines = file($file);
+
+		if ($lines === false)
+		{
+			return false;
+		}
+
 		$nLines = count($lines);
 		$keys = array();
 
