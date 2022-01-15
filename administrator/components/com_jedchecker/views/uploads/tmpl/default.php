9d12ef26e19e6ae90f11d636818b2362fac207f4
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2022-01-15 15:56:04 +0100

    Update default.php

--- administrator/components/com_jedchecker/views/uploads/tmpl/default.php
+++ administrator/components/com_jedchecker/views/uploads/tmpl/default.php
@@ -44,7 +44,7 @@ else
 JHtml::stylesheet('media/com_jedchecker/css/style' . $cssSuffix);
 JHtml::script('media/com_jedchecker/js/script.js');
 ?>
-<script id="jed-rules-json" type="application/json"><?php echo json_encode($this->jsOptions); ?></script?
+<script id="jed-rules-json" type="application/json"><?php echo json_encode($this->jsOptions); ?></script>
 <div id="jedchecker">
 	<div class="row g-3">
 		<div class="col-12 col-md-8">
